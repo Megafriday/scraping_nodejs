db3682a9ee1dbdd9cc8d3b2b0cb59926c7a4fdd4
megafriday <megafriday@yahoo.co.jp>
2019-10-16 02:12:40 +0900

    Inital Commit

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 const request = require('request');
 const {JSDOM} = require('jsdom');
 
-const selector = "#primary > div > div > div > article > header > h2 > a";
-
 const urls = [];
 urls.push("https://www.sejuku.net/blog/category/programing/node-js");
 urls.push("https://www.sejuku.net/blog/category/programing/node-js/page/2");
@@ -27,8 +25,10 @@ function printTitle(url){
 		
 		try {
 			const dom = new JSDOM(body);
-			const aList = dom.window.document.querySelectorAll(selector);
 
+			const selector = "#primary > div > div > div > article > header > h2 > a";
+			const aList = dom.window.document.querySelectorAll(selector);
+			
 			aList.forEach((a)=>{
 				console.log(a.textContent);
 			});
